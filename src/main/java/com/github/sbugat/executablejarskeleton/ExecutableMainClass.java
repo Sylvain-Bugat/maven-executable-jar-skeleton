ab9c3be619ebf0773558977b51f7b9483710ad16
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-04-13 21:57:09 +0200

    Add no display mode and update jar files

--- src/main/java/com/github/sbugat/executablejarskeleton/ExecutableMainClass.java
+++ src/main/java/com/github/sbugat/executablejarskeleton/ExecutableMainClass.java
@@ -1,25 +1,31 @@
 package com.github.sbugat.executablejarskeleton;
 
+import java.awt.HeadlessException;
 import java.io.IOException;
 
 import javax.swing.JOptionPane;
 
 /**
  * Main class of the Executable jar
- *
+ * 
  * @author Sylvain Bugat
- *
+ * 
  */
 public class ExecutableMainClass {
 
 	/**
 	 * Main program launched in the jar file
-	 *
+	 * 
 	 * @param args
 	 * @throws IOException
 	 */
-	public static void main( final String args[] ) throws IOException{
+	public static void main(final String args[]) {
 
-		JOptionPane.showMessageDialog( null, "It works!", "Your skill is great!!", JOptionPane.INFORMATION_MESSAGE ); //$NON-NLS-1$ //$NON-NLS-2$
+		try {
+			JOptionPane.showMessageDialog(null, "It works!", "Your skill is great!!", JOptionPane.INFORMATION_MESSAGE); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		catch (final HeadlessException headlessException) {
+			System.out.println("No display mode: It works! Your skill is great!!");
+		}
 	}
 }
